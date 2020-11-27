fb78ede04fb6f59588c5e32f831366d5d868417c
ldiebold <luke@ldiebold.com>
2020-11-27 21:47:57 +1100

    WIP

--- src/sanctum.conf.js
+++ src/sanctum.conf.js
@@ -2,5 +2,5 @@ export default {
   login_url: 'https://api.process.test/login',
   logout_url: 'https://api.process.test/logout',
   user_endpoint: process.env.API_URL + '/api/user',
-  csrf_cookie: process.env.API_URL + '/sanctum/csrf-cookie'
+  csrf_cookie_url: process.env.API_URL + '/sanctum/csrf-cookie'
 }
