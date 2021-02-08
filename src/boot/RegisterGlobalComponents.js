6d5e5993870339b246129d68856f9984cad1972f
ldiebold <luke@ldiebold.com>
2021-02-08 21:50:11 +1100

    ran build

--- src/boot/RegisterGlobalComponents.js
+++ src/boot/RegisterGlobalComponents.js
@@ -1,5 +1,5 @@
-import { MShowTutorialVideoButton } from '@ldiebold/quasar-ui-process-model-components/src'
+// import { MShowTutorialVideoButton } from '@ldiebold/quasar-ui-process-model-components/src'
 
-export default async ({ Vue }) => {
-  Vue.component('MShowTutorialVideoButton', MShowTutorialVideoButton)
-}
+// export default async ({ Vue }) => {
+//   Vue.component('MShowTutorialVideoButton', MShowTutorialVideoButton)
+// }
